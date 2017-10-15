51d613da9c06495d24d08ffa4d6a6727164387bf
Ox Cart <ox.to.a.cart@gmail.com>
2017-10-15 14:51:10 -0500

    Updated pubsub broker to renew its own letsencrypt certs

--- broker/main.go
+++ broker/main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/getlantern/pubsub"
-	"github.com/getlantern/tlsdefaults"
 	"github.com/golang/glog"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
 	addr      = flag.String("addr", ":14443", "The address at which to listen for TLS connections")
 	httpsaddr = flag.String("httpsaddr", ":62443", "The address at which to listen for HTTPS connections")
-	pkfile    = flag.String("pkfile", "pk.pem", "Path to the private key PEM file")
-	certfile  = flag.String("certfile", "cert.pem", "Path to the certificate PEM file")
 	authkey   = flag.String("authkey", "", "The authentication key to use for authenticating publishers")
 )
 
@@ -28,13 +27,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	l, err := tlsdefaults.Listen(*addr, *pkfile, *certfile)
+	m := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache("certs"),
+		Email:  "admin@getlantern.org",
+	}
+	tlsConfig := &tls.Config{
+		GetCertificate:           m.GetCertificate,
+		PreferServerCipherSuites: true,
+	}
+
+	l, err := tls.Listen("tcp", *addr, tlsConfig)
 	if err != nil {
 		glog.Fatalf("Unable to listen: %v", err)
 	}
 	fmt.Fprintf(os.Stdout, "Listening for TLS connections at %v\n", l.Addr())
 
-	hl, err := tlsdefaults.Listen(*httpsaddr, *pkfile, *certfile)
+	hl, err := tls.Listen("tcp", *httpsaddr, tlsConfig)
 	if err != nil {
 		glog.Fatalf("Unable to listen HTTPS: %v", err)
 	}
